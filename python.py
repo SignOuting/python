db5387f0a7e34e05a70f7ffbe1bdc3083c2c4726
SignOuting <asalexandrov11@icloud.com>
2024-12-11 10:42:34 +0300

    Add files via upload

---
+++ python.py
@@ -0,0 +1,6 @@
+d = int(input())
+x = int(input())
+a = ((365 - (75 / (d ** 3))) / (3 * (d ** 2) - d)) * 5
+b = ((412 - (125 / (d ** 3))) / (2 * (d ** 2) - d)) * 4
+
+print(round(a * x) + round(b * x))
